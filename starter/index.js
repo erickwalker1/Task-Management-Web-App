233554faa1f6e8f72280e2bcf4fc6e3e7d773ab2
erickwalker1 <walkererick1@gmail.com>
2022-08-15 16:25:00 -0400

    Theme Switch Implementation & Code Cleanup

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 const hideSidebarBtn = document.querySelector(".toggle-hide");
+const checkbox = document.querySelector("input[name=theme]");
 
 hideSidebarBtn.addEventListener("click", (event) => {
   event.preventDefault();
@@ -7,3 +8,21 @@ hideSidebarBtn.addEventListener("click", (event) => {
     ? container.classList.replace("container", "container-hide-sidebar")
     : container.classList.replace("container-hide-sidebar", "container");
 });
+
+// <!--- Toggle Theme --->
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    transition();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    transition();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+const transition = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
