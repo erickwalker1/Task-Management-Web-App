161d95eb62a2fbdfce0bbcb41ec4453b7f604411
erickwalker1 <walkererick1@gmail.com>
2022-08-18 07:59:43 -0400

    Hover State & Hide/Show Sidebar Implementation

--- starter/index.js
+++ starter/index.js
@@ -1,12 +1,18 @@
 const container = document.querySelector(".container");
 const hideSidebarBtn = document.querySelector(".toggle-hide");
 const checkbox = document.querySelector("input[name=theme]");
+const viewBtn = document.querySelector(".btn-toggle-view");
 
+// <!--- Toggle Sidebar View: Hide --->
 hideSidebarBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  container.classList.contains("container")
-    ? container.classList.replace("container", "container-hide-sidebar")
-    : container.classList.replace("container-hide-sidebar", "container");
+  container.classList.toggle("active");
+});
+
+// <!--- Toggle Sidebar View: Show --->
+viewBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  container.classList.toggle("active");
 });
 
 // <!--- Toggle Theme --->
@@ -20,6 +26,7 @@ checkbox.addEventListener("change", function () {
   }
 });
 
+// <!--- Toggle Theme: Transition Effect --->
 const transition = () => {
   document.documentElement.classList.add("transition");
   window.setTimeout(() => {
