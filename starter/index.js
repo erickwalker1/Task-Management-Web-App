e8d23ae16c0d976fa5c2ce9c1ea7aa22189e3bcb
erickwalker1 <walkererick1@gmail.com>
2022-08-28 15:10:25 -0400

    Theme Setting LocalStorage

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 const hideSidebarBtn = document.querySelector(".toggle-hide");
-const checkbox = document.querySelector("input[name=theme]");
+const themeSwitch = document.getElementById("themeSwitch");
 const viewBtn = document.querySelector(".btn-toggle-view");
 
 // <!--- Toggle Sidebar View: Hide --->
@@ -16,15 +16,27 @@ viewBtn.addEventListener("click", (event) => {
 });
 
 // <!--- Toggle Theme --->
-checkbox.addEventListener("change", function () {
-  if (this.checked) {
+const resetTheme = function () {
+  if (themeSwitch.checked) {
     transition();
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("themeSwitch", "dark"); // setting theme in local storage
   } else {
     transition();
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("themeSwitch", "light"); // setting theme in local storage
   }
-});
+};
+
+const initTheme = function () {
+  const themeSelected = localStorage.getItem("themeSwitch"); // getting theme's value in local storage
+  document.documentElement.setAttribute("data-theme", themeSelected);
+};
+
+if (themeSwitch) {
+  initTheme();
+  themeSwitch.addEventListener("change", resetTheme);
+}
 
 // <!--- Toggle Theme: Transition Effect --->
 const transition = () => {
